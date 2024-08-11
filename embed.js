efec2770ccec36b63dc3bd5512a11c25e27f1eb5
KJZH001 <2802007619@qq.com>
2024-08-11 12:40:16 +0000

    移除按钮，分离去主题做适配

--- embed.js
+++ embed.js
@@ -719,26 +719,26 @@ class LeaflowAmber {
 //   <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
 //   <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2"/>
 // </svg></button>-->
-<div style="
-        bottom: 210px;
-        right: 20px;
-        position: fixed;
-        z-index: 999;
-        display: table;
-        background: #848484;
-        width: 40px;
-        height: 40px;
-        text-align: center;">
-    <a style="
-        color: #fff; 
-        display: inline-block; 
-        width: 100%; 
-        height: 100%; 
-        line-height: 40px; 
-        vertical-align: middle;" id="moe-leaflow-amber-show-chat-button">
-        <span class="fa fa-pencil" style="line-height: 1;"></span>
-    </a>
-</div>
+// <div style="
+//         bottom: 210px;
+//         right: 20px;
+//         position: fixed;
+//         z-index: 999;
+//         display: table;
+//         background: #848484;
+//         width: 40px;
+//         height: 40px;
+//         text-align: center;">
+//     <a style="
+//         color: #fff; 
+//         display: inline-block; 
+//         width: 100%; 
+//         height: 100%; 
+//         line-height: 40px; 
+//         vertical-align: middle;" id="moe-leaflow-amber-show-chat-button">
+//         <span class="fa fa-pencil" style="line-height: 1;"></span>
+//     </a>
+// </div>
     <div id="leaflow-amber-chat" style="display: none;">
         <div class="leaflow-amber-chat-container" id="leaflow-amber-chat-container">
             <div class="leaflow-amber-header">
